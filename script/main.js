8aee617218f90c2b936979624c0ecbec9f04bcfb
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-05-22 02:07:44 +0530

    mesh animation init

--- script/main.js
+++ script/main.js
@@ -18,22 +18,30 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
 var geometry = new THREE.BoxGeometry(1,1,1); // (1,10,10) (1,4,10) (1,4,4)
-var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
-var mesh = new THREE.Mesh(geometry, material);
+var material = new THREE.MeshLambertMaterial({color: 0xF7F7F7});
+// var mesh = new THREE.Mesh(geometry, material);
 
-// mesh.position.x = -2;
-// mesh.position.y = 2;
-// mesh.position.z = 2;
-// mesh.position.set(2,2,-2);
+meshX = -10;
 
-// mesh.rotation.set(45,0,0);
-// mesh.scale.set(1,2,1);
+for(var i = 0; i < 15; i++){
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 10;
+    mesh.position.y = (Math.random() - 0.5) * 10;
+    mesh.position.z = (Math.random() - 0.5) * 10;
+    scene.add(mesh);
+    meshX += 1;
+}
 
-scene.add(mesh);
+var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+light.position.set(0,0,0);
+scene.add(light);
 
-var light = new THREE.PointLight(0xFFFFFF, 1, 500);
-light.position.set(10,0,25);
+var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
+light.position.set(0,0,25);
 scene.add(light);
 
 var render = function() {
@@ -47,15 +55,25 @@ var render = function() {
     renderer.render(scene, camera);
 }
 
+function onMouseMove(event) {
+    event.preventDefault();
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1; 
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1; 
+
+    raycaster.setFromCamera(mouse, camera);
+
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    for(var i = 0; i < intersects.length; i++){
+    //    intersects[i].object.material.color.set(0xff0000);
+        this.tl = new TimelineMax(); // .delay(.3)
+        this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut});
+        this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut});
+        this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut});
+        this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5"); 
+    }
+} 
+
+
+window.addEventListener('mousemove', onMouseMove);
 render();
-
-
-this.tl = new TimelineMax({paused: true}); // .delay(.3)
-this.tl.to(this.mesh.scale, 1, {x: 2, ease: Expo.easeOut});
-this.tl.to(this.mesh.scale, .5, {x: .5, ease: Expo.easeOut});
-this.tl.to(this.mesh.position, .5, {x: 2, ease: Expo.easeOut});
-this.tl.to(this.mesh.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5");
-
-document.body.addEventListener('click', () => {
-    this.tl.play();
-})
